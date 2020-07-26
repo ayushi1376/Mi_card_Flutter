b7c70d225f1f327a7db5f025ddcbf41b74e79fcb
Ayushi Verma <yuktaverma2005@gmail.com>
2020-07-26 17:11:00 +0530

    Third commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.pinkAccent,
         body: SafeArea(
           child: Column(
             children: <Widget>[
@@ -22,10 +22,61 @@ class MyApp extends StatelessWidget {
                 "Ayushi Verma",
                 style: TextStyle(
                   fontSize: 40.0,
-                  color: Colors.lightGreen,
+                  color: Colors.black45,
                   fontFamily: 'Dancing_Script',
-
-
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              Text(
+                'ENGINEERING STUDENT',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.black,
+                  fontFamily: 'Source Sans Pro',
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.5,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                        Icons.phone,
+                    ),
+                    SizedBox(width: 10.0,),
+                    Text(
+                      '+91 8091198977',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                    ),
+                    SizedBox(width: 10.0,),
+                    Text(
+                      'ayushiiverma80@gmail.com',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
